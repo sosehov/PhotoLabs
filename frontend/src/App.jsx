7d9f1bbcbdbcf253e382814354eb821024a713ef
Soseh <sosehv@gmail.com>
2025-04-04 19:42:22 -0400

    Removes sampleDataForPhotoList array

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,58 +3,6 @@ import PhotoList from './components/PhotoList';
 import './App.scss';
 
 const App = () => {
-
-  const sampleDataForPhotoList = [
-    {
-      id: 1,
-      location: {
-        city: "Montreal",
-        country: "Canada",
-      },
-      urls: {
-        full: "/Image-1-Full.jpeg",
-        regular: "/Image-1-Regular.jpeg",
-      },
-      user: {
-        username: "exampleuser",
-        name: "Joe Example",
-        profile: "/profile-1.jpg",
-      },
-    },
-    {
-      id: 2,
-      location: {
-        city: "Toronto",
-        country: "Canada",
-      },
-      urls: {
-        full: "/Image-2-Full.jpeg",
-        regular: "/Image-2-Regular.jpeg",
-      },
-      user: {
-        username: "exampleuser",
-        name: "Joe Example",
-        profile: "/profile-1.jpg",
-      },
-    },
-    {
-      id: 3,
-      location: {
-        city: "Ottawa",
-        country: "Canada",
-      },
-      urls: {
-        full: "/Image-3-Full.jpeg",
-        regular: "/Image-3-Regular.jpeg",
-      },
-      user: {
-        username: "exampleuser",
-        name: "Joe Example",
-        profile: "/profile-1.jpg",
-      },
-    },
-  ];
-
   return (
     <div className="app">
       {/* sampleDataForPhotoList.map((photoData) => (
