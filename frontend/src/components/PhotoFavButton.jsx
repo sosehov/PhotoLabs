f62dbefedabd5008a8082df72968ef3b0e28d4aa
Soseh <sosehv@gmail.com>
2025-04-04 13:33:09 -0400

    Adds PhotoFavButton component functionality and updates PhotoListItem to pass down props

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,12 +1,16 @@
-// import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-const PhotoFavButton = () => {
+const PhotoFavButton = (props) => {
+  const { isLiked, handleClick } = props;
+
   return (
-    <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
-      </div>
+    <div className="photo-list__fav-icon" onClick={handleClick}>
+      <svg 
+        className={`photo-list__fav-icon-svg ${isLiked ? 'liked' : ''}`} 
+        viewBox="0 0 24 24"
+      >
+        <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5c0-2.5 2-4.5 4.5-4.5S11 6 12 6s3.5-1.5 3.5-3.5S16 2.5 16 2.5s3.5 0 3.5 4.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+      </svg>
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,13 +1,25 @@
+import React from "react";
+import react, { useState } from 'react';
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
   const { username, imageSource, id, location, profile} = props;
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleClick = () => {
+    setIsLiked(!isLiked);
+  };
 
   return (
     <div className="photo-list__item">
       <img src={imageSource} className="photo-list__image" />
       <div className="photo-list__user-details">
         <img src={profile} alt={`Profile of ${username}`} className="photo-list__user-profile" />
+        <PhotoFavButton 
+          isLiked={isLiked} 
+          handleClick={handleClick} 
+        />
         <div className="photo-list__user-info">
           <h3>{username}</h3>
           <div className="photo-list__user-location">
