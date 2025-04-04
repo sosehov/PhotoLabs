082fcf79812d9140edc8654ecc0b689dec4245a6
Soseh <sosehv@gmail.com>
2025-04-04 11:37:34 -0400

    Adds array helper function to display three images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -77,13 +77,16 @@ const App = () => {
 
   return (
     <div className="app">
-      <PhotoListItem 
-        username={sampleDataForPhotoListItem.user.username}
-        imageSource={sampleDataForPhotoListItem.urls.full}
-        id={sampleDataForPhotoListItem.id}
-        location={sampleDataForPhotoListItem.location}
-        profile={sampleDataForPhotoListItem.user.profile}
-      />
+      {sampleDataForPhotoList.map((photoData) => (
+        <PhotoListItem 
+          key={photoData.id} //Add a key for each list item
+          username={photoData.user.username}
+          imageSource={photoData.urls.full}
+          id={photoData.id}
+          location={photoData.location}
+          profile={photoData.user.profile}
+        />
+      ))}
       <PhotoList />
 
     </div>
