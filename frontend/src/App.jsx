8ce64fea2089b6367aa2eb9e9424c5896e49e923
Soseh <sosehv@gmail.com>
2025-04-05 19:30:03 -0400

    Adds useState() and useEffect() and passes likedPhotos and toggleFavorite to HomeRoute component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React , { useEffect, useState } from 'react';
 import HomeRoute from './routes/HomeRoute';
 import topics from './mocks/topics';
 import photos from './mocks/photos';
 import './App.scss';
 
 const App = () => {
+  const [likedPhotos, setLikedPhotos] = useState([]);
+
+  useEffect(() => {
+    // placeholder for future
+  }, []);
+
+  const toggleFavorite = (photoId) => {
+    setLikedPhotos((prevLikedPhotos) => {
+      if (prevLikedPhotos.includes(photoId)) {
+        // Remove from liked photos if it's already in the list
+        return prevLikedPhotos.filter((id) => id !== photoId);
+      } else {
+        // Add to liked photos if it's not in the list
+        return [...prevLikedPhotos, photoId];
+      }
+    });
+  };
+
   return (
     <div className="app">
       <HomeRoute 
         photos={photos}
         topics={topics}
+        likedPhotos={likedPhotos}
+        toggleFavorite={toggleFavorite}
       />
     </div>
   );
