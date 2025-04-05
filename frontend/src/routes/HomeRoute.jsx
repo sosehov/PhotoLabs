0a5d9dfa4a761a8c1765b111d0c99155be938a37
Soseh <sosehv@gmail.com>
2025-04-05 19:38:40 -0400

    Gets likedPhotos and toggleFavorite from props and passes them to PhotoList component

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -3,12 +3,15 @@ import PhotoList from '../components/PhotoList';
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = (props) => {
-  const {photos, topics} = props;
+  const {photos, topics, likedPhotos, toggleFavorite} = props;
   
   return (
     <div className="home-route">
       <TopNavigationBar topics={topics}/>
-      <PhotoList photos={photos}/>
+      <PhotoList 
+        photos={photos}
+        likedPhotos={likedPhotos}
+        toggleFavorite={toggleFavorite}/>
     </div>
   );
 };
