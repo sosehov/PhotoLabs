3f00fb03304cd8ede5266393ee7d5f8ab6e8b806
Soseh <sosehv@gmail.com>
2025-04-04 23:35:06 -0400

    Adds TopNavigationBar and PhotoList components to HomeRoute()

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,9 +1,11 @@
+import TopNavigationBar from '../components/TopNavigationBar';
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = () => {
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigationBar />
+      <PhotoList />
     </div>
   );
 };
