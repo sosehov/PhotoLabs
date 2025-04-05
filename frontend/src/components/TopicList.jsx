592dcbe001fa59fe6baf295700f1d7930a05e1ba
Soseh <sosehv@gmail.com>
2025-04-04 23:53:10 -0400

    Imports and uses topics mock data and removes sampleDataForTopicList array

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,28 +1,11 @@
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
+import topics from '../mocks/topics';
 
-const sampleDataForTopicList = [
-  {
-    id: 1,
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: 2,
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: 3,
-    slug: "topic-3",
-    title: "People",
-  },
-];
-
-const TopicList = (props) => {
+const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map((topicData) => (
+      {topics.map((topicData) => (
         <TopicListItem 
           key={topicData.id}
           id={topicData.id}
