8369cc01ba7802669e98d79e417b0b925881d785
Soseh <sosehv@gmail.com>
2025-04-04 22:46:45 -0400

    Adds a span and <h2> to the return statement for showing topic title

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,15 +1,13 @@
 import "../styles/TopicListItem.scss";
 
-const sampleDataForTopicListItem = {
-  id: 1,
-  slug: "topic-1",
-  label: "Nature",
-};
+const TopicListItem = (props) => {
+  const {title} = props;
 
-const TopicListItem = () => {
   return (
     <div className="topic-list__item">
-      {/* Insert React */}
+      <span className="topic-list__item span">
+        <h2>{title}</h2>
+      </span>
     </div>
   );
 };
