9b0256d6287e3d821f8de9417ef8cd73915b9cba
Soseh <sosehv@gmail.com>
2025-04-05 00:16:16 -0400

    Adds getting props from App component and passing them to TopNavigationBar and PhotoList components

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -2,11 +2,13 @@ import TopNavigationBar from '../components/TopNavigationBar';
 import PhotoList from '../components/PhotoList';
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = () => {
+const HomeRoute = (props) => {
+  const {photos, topics} = props;
+  
   return (
     <div className="home-route">
-      <TopNavigationBar />
-      <PhotoList />
+      <TopNavigationBar topics={topics}/>
+      <PhotoList photos={photos}/>
     </div>
   );
 };
