73e9ec17e924509cf7971a040093886a3f0851c2
Soseh <sosehv@gmail.com>
2025-04-05 00:11:25 -0400

    Passes props to HomeRoute and importing mockdata to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import HomeRoute from './routes/HomeRoute';
+import topics from './mocks/topics';
+import photos from './mocks/photos';
 import './App.scss';
 
 const App = () => {
   return (
     <div className="app">
-      <HomeRoute />
+      <HomeRoute 
+        photos={photos}
+        topics={topics}
+      />
     </div>
   );
 };
