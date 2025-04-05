4c021d46de3d410a0260ae5aae247a3b10148ca5
Soseh <sosehv@gmail.com>
2025-04-05 00:13:55 -0400

    Adds getting props from HomeRoute and passing it to TopicList

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -2,11 +2,11 @@ import '../styles/TopNavigationBar.scss'
 import FavBadge from './FavBadge';
 import TopicList from './TopicList';
 
-const TopNavigationBar = () => {
+const TopNavigationBar = ({ topics }) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList />
+      <TopicList topics={topics}/>
       <FavBadge />
     </div>
   )
