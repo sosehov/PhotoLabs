7cd2466436002e7cf4a8b91c44ca338112047722
Soseh <sosehv@gmail.com>
2025-04-06 16:17:18 -0400

    Adds logic to display selected photo in larger size and renders PhotoList inside the modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,23 +1,57 @@
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
+import PhotoList from '../components/PhotoList';
 
-const PhotoDetailsModal = ({closeModal, photo}) => {
+const PhotoDetailsModal = ({photoData, similarPhotos, closeModal}) => {
+  const {user: { username, profile }, urls: { full: imageSource }, location } = photoData
+
   return (
-    <>
-      <div className="photo-details-modal">
-        <button className="photo-details-modal__close-button" onClick={closeModal}>
-          <img src={closeSymbol} alt="close symbol" />
-          {console.log(photo)}
-        </button>
+    <div className="photo-details-modal">
+      <button 
+        className="photo-details-modal__close-button" 
+        onClick={closeModal}
+      >
+        <img src={closeSymbol} alt="close symbol" />
+      </button>
+
+      <div className="photo-details-modal__top-bar">
+        <div className="photo-details-modal__photographer-details">
+          <img 
+            src={profile} 
+            alt={`Profile of ${username}`} 
+            className="photo-details-modal__photographer-profile"
+          />
+          <div className="photo-details-modal__photographer-info">
+            <h3>{username}</h3>
+            <div className="photo-details-modal__photographer-location">
+              <p>{location.city}, {location.country}</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className='photo-details-modal__image'>
-        <img src={photo} />
+
+      {/* Display the selected photo in larger size */}
+      <div className="photo-details-modal__images">
+        <img 
+          src={imageSource} 
+          alt={`Photo by ${username}`} 
+          className="photo-details-modal__image"
+        />
       </div>
-      <div className='photo-details-modal__images'>
+
+      {/* Section for similar photos */}
+      <div className="photo-details-modal__similar-photos">
+        <h4 className="photo-details-modal__header">Similar Photos</h4>
         
+        {/* Render the PhotoList component to show similar photos */}
+        <PhotoList 
+          photos={similarPhotos} 
+          likedPhotos={[]} // Passing empty array for likedPhotos; could be extended later
+          toggleFavorite={() => {}} // Placeholder, modify to handle favorite toggling if needed
+        />
       </div>
-    </>
-  )
+    </div>
+  );
 };
 
 export default PhotoDetailsModal;
