14bc15b4d2003f5484067cf7084038fe4277ba37
Soseh <sosehv@gmail.com>
2025-04-05 23:01:58 -0400

    Gets openModal props and passes it to PhotoListItem

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,18 +1,19 @@
 import "../styles/PhotoList.scss";
 import PhotoListItem from './PhotoListItem';
 
-const PhotoList = ({ photos, likedPhotos, toggleFavorite }) => {
+const PhotoList = ({ photos, likedPhotos, toggleFavorite, openModal }) => {
   return (
-    <ul className="photo-list">
-      {photos.map((photoData) => (
+    <div className="photo-list">
+      {photos.map((photo) => (
         <PhotoListItem
-          key={photoData.id}
-          photoData={photoData}
+          key={photo.id}
+          photoData={photo}
           likedPhotos={likedPhotos}
           toggleFavorite={toggleFavorite}
+          openModal={openModal}
         />
       ))}
-    </ul>
+    </div>
   );
 };
 
