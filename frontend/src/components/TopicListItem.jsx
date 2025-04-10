4423450bc111049b772b24d2bfcb063739ba6991
Soseh <sosehv@gmail.com>
2025-04-10 13:15:47 -0400

    Adds handleclick() and passes it to onClick

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -3,8 +3,12 @@ import "../styles/TopicListItem.scss";
 const TopicListItem = ({topicData}) => {
   const {title} = topicData;
 
+  const handleClick = () => {
+    onClick(id);
+  }
+
   return (
-    <div className="topic-list__item">
+    <div className="topic-list__item" onClick={handleClick} role="button" tabIndex={0}>
       <span className="topic-list__item span">
         <h2>{title}</h2>
       </span>
